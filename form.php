2535d35340524e3e6804e77c345d8bf615a135fb
Matheus Käld <Matheus.kald@axxell.fi>
2022-11-08 11:27:25 +0200

    har ändrat mycket css och footer

--- form.php
+++ form.php
@@ -1,4 +1,12 @@
-<link rel="stylesheet" href="FORMstyle.css">
+<head>
+<title> Programmering - Lånekalkylator </title>
+<!-- Set charset to allow ÅÄÖ -->
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/FORMstyle.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</head>
+
 
 <div id="wholeting">
 
@@ -8,7 +16,7 @@ include 'includes/config.php';
 
 ?>
 
-<link rel="stylesheet" href="style.css">
+<div id="formcss">
 <form method="post" action="create_article.php">
   <label for="Fornamn">Förnamn</label><br>
   <input class="colorstuff" type="text" id="Fornamn" name="Fornamn" value=""><br><br>
@@ -24,23 +32,23 @@ include 'includes/config.php';
   <label for="Tid">Tid</label><br>
   <input class="colorstuff" type="text" id="Tid" name="Tid" value=""><br><br>
   
-  <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Typ av massage
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div><br>
+  <label for="cars">Typ av massage:</label><br>
+
+<select name="cars" id="massages">
+  <option value="volvo">klassisk</option>
+  <option value="saab">Sport massage</option>
+  <option value="mercedes">Fysio therapi</option>
+</select><br><br>
+
+
   
   <input id="date" type="date"><br><br>
+
   <input id="submit" type="submit" name="article-submit" value="Send">
 
 
 </form>
-
+</div>
 
 </div>
 
