e36ef3c6c5ab9ed1013ecc35b62bb200518b1c61
Matheus Käld <Matheus.kald@axxell.fi>
2022-11-08 09:46:31 +0200

    lagade footer

--- form.php
+++ form.php
@@ -1,6 +1,14 @@
 <link rel="stylesheet" href="style.css">
 
+<div id="wholeting">
 
+<?php
+include 'includes/config.php';
+
+
+?>
+
+<link rel="stylesheet" href="style.css">
 <form method="post" action="create_article.php">
   <label for="Fornamn">Förnamn</label><br>
   <input class="colorstuff" type="text" id="Fornamn" name="Fornamn" value=""><br><br>
@@ -16,8 +24,23 @@
   <label for="Tid">Tid</label><br>
   <input class="colorstuff" type="text" id="Tid" name="Tid" value=""><br><br>
   
-  <input type="date">
-  <input id="submit" type="submit" name="article-submit" value="Send">
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Typ av massage
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div><br>
   
+  <input id="date" type="date"><br><br>
+  <input id="submit" type="submit" name="article-submit" value="Send">
+
+
 </form>
+
+
+</div>
 
