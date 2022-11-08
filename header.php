37c7a7a967d69c529d59bb77b3478cf5af444d70
AmiGo <amiarabid05@gmail.com>
2022-11-08 11:16:48 +0200

    header o style

--- header.php
+++ header.php
@@ -20,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Bookings</a>
+          <a class="nav-link active" aria-current="page" href="form.php">Bookings</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Reservation§</a>
+          <a class="nav-link" href="#">Reservation</a>
         </li>
     </ul>
     </div>
